4e9be61f366bda06b81370336bff70ec4e5eda86
xwlyy <xwlyy1991@gmail.com>
2016-09-04 21:52:01 +0800

    deploy my blog

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
   ? 'https://express-vue-blog.herokuapp.com/'
-  : 'https://express-vue-blog.herokuapp.com/'
+  : 'http://localhost:3000/'
