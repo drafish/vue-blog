78fa67ba742059bfea617e8ffbb51b7a007b6b18
xwlyy <xwlyy1991.gmail.com>
2017-07-25 09:00:45 +0800

    添加yarn.lock

--- src/components/Vfooter.vue
+++ src/components/Vfooter.vue
@@ -2,8 +2,8 @@
     <div class="uk-margin-large-top" style="background-color:#eee; border-top:1px solid #ccc;">
         <div class="uk-container uk-container-center uk-text-center">
             <div class="uk-panel uk-margin-top uk-margin-bottom">
-                
-                <p>Powered by <a href='https://vuejs.org.cn/'>Vue</a>|<a href='http://getuikit.net/'>Uikit</a>|<a href='http://expressjs.com/zh-cn/'>Express</a>. Copyright &copy; 2016</p>
+
+                <p>Powered by <a href='https://vuejs.org.cn/'>Vue</a>|<a href='http://getuikit.net/'>Uikit</a>|<a href='https://thinkjs.org/'>Thinkjs</a>. Copyright &copy; 2017</p>
             </div>
 
         </div>
